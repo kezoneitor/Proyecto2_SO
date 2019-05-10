e54ba4b5d4d8a3ebd6d429124d1bbf6ba9e9ea20
Unknown <keslerth.c@gmail.com>
2019-05-10 17:02:15 -0600

    Filtros, interfaz server

--- OpenCV/filtros.py
+++ OpenCV/filtros.py
@@ -2,13 +2,6 @@ import cv2
 import numpy as np
 import imageio
 
-#variable de nombre de archivo
-nombre_archivo ="./data/second_0.jpg"
-
-# Reading in and displaying our image
-image = cv2.imread(nombre_archivo)
-cv2.imshow('Original', image)
-
 #filtro para hacer nitidez a la imagen
 def nitidez(img):
     # Create our shapening kernel, it must equal to one eventually
@@ -17,9 +10,7 @@ def nitidez(img):
                                   [-1,-1,-1]])
     # applying the sharpening kernel to the input image & displaying it.
     sharpened = cv2.filter2D(img, -1, kernel_sharpening)
-    cv2.imshow('Imagen nitida', sharpened)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    return sharpened
 
 #para calcular si es muy clara o oscura la imagen
 def calcular_imagen(filename, thrshld):
@@ -33,35 +24,44 @@ def calcular_imagen(filename, thrshld):
 
 #filtro de aclarar
 def aclarar(img,increase):
-
     image = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
     v = image[:, :, 2]
     v = np.where(v <= 255 - increase, v + increase, 255)
     image[:, :, 2] = v
-
     image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
-
     cv2.imshow('Brightness', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
 
 
 #filtro para oscurecer la imagen
 def oscurecer(img,valor):
-
     hsvImg = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
     hsvImg[..., 2] = hsvImg[..., 2] * valor
-
     image = cv2.cvtColor(hsvImg, cv2.COLOR_HSV2BGR)
-
     cv2.imshow('Darkness', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
 
 #calcular_imagen(nombre_archivo,127)
 # aclarar(image,30)
-oscurecer(image, 0.6)
+#oscurecer(image, 0.6)
 #nitidez(image)
 
+def ejecutarCrearImagenes(nombre_archivo, guardar_en, cantFrames, videoFps):
+    video = cv2.VideoCapture(nombre_archivo)
+    print(video)
+    i=0
+    while (True):
+        # Capture frame-by-frame
+        ret, frame = video.read()
+        # Condiciones de salida >> Presionar la letra 'q' o que ya no existen mas frames
+        if cv2.waitKey(20) & 0xFF == ord('q') or not(ret):
+            break
+        cv2.imshow('frame',frame)
+        if(i%(int(videoFps/cantFrames)) == 0):
+            cv2.imwrite(guardar_en+'/second_'+str(i)+'.jpg',frame)
+        i+=1
+    # When everything done, release the capture
+    video.release()
+    cv2.destroyAllWindows()
+
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+ejecutarCrearImagenes("tkwWilliam.mp4", "data", 2, 30)
--- OpenCV/main.py
+++ OpenCV/main.py
@@ -2,22 +2,23 @@ from tkinter import *
 from tkinter import font
 from tkinter.filedialog import askopenfilename
 from tkinter.messagebox import showerror
+from assets.colors import *
 
 class MyFrame(Frame):
     def __init__(self):
         Frame.__init__(self)
         self.master.title("Panel disrruptivo")
-        self.master.geometry("900x200")
-        self.master.configure(background="SpringGreen3")
+        self.master.geometry("800x100")
+        self.master.configure(background=coPrincipal)
         self.master.rowconfigure(10, weight=1)
         self.master.columnconfigure(10, weight=1)
         #Fonts ========================================================================================
-        self.titleFont = font.Font(family="Console", size=12, weight="bold")
+        self.subtitleFont = font.Font(family="Console", size=12, weight="bold")
         self.normalFont = font.Font(family="Console", size=10)
         #Labels =======================================================================================
-        self.lblbtnLoad = Label(self.master, text="Load video", font=self.titleFont, background="SpringGreen3")
-        self.lblTextLoad = Label(self.master, text="File name", font=self.normalFont, background="SpringGreen3")
-        self.lblAddress = Label(self.master, text="Address", font=self.titleFont, background="SpringGreen3")
+        self.lblbtnLoad = Label(self.master, text="Load video", font=self.subtitleFont, background=coPrincipal)
+        self.lblTextLoad = Label(self.master, text="File name", font=self.normalFont, background=coPrincipal)
+        self.lblAddress = Label(self.master, text="Address", font=self.subtitleFont, background=coPrincipal)
         #Buttons ======================================================================================
         self.btnLoad = Button(self.master,
                               text="Browse",
@@ -33,19 +34,18 @@ class MyFrame(Frame):
         self.btnCreateImages = Button(self.master, text="Eject", font=self.normalFont, background="light gray")
         self.btnQuality = Button(self.master, text="Increment Quality", font=self.normalFont, background="light gray")
         #Positions ====================================================================================
-        #   >> Row 0
-        self.lblbtnLoad.grid(row=0, column=0)
-        self.btnLoad.grid(row=0, column=1)
-        self.lblTextLoad.place(x=200, y=5)
-        self.btnCreateImages.place(x=200, y=40)
-        self.btnQuality.place(x=250, y=40)
-        Label(self.master, text="", font=self.titleFont, background="SpringGreen3").grid(row=1,column=0)
-        Label(self.master, text="", font=self.titleFont, background="SpringGreen3").grid(row=2,column=0)
+        #   >> Row 1
+        self.lblAddress.grid(row=1, column=0)
+        self.inputAnddress.grid(row=1, column=1)
+        self.btnConnect1.grid(row=1, column=2)
+        self.btnConnect2.grid(row=1, column=3)
         #   >> Row 2
-        self.lblAddress.grid(row=3, column=0)
-        self.inputAnddress.grid(row=3, column=1)
-        self.btnConnect1.grid(row=3, column=2)
-        self.btnConnect2.grid(row=3, column=3)
+        self.lblbtnLoad.grid(row=2, column=0)
+        self.btnLoad.grid(row=2, column=1)
+        self.lblTextLoad.place(x=200, y=30)
+        self.btnCreateImages.place(x=100, y=65)
+        self.btnQuality.place(x=150, y=65)
+
     def load_file(self):
         fname = askopenfilename(filetypes=(("MP4 files", "*.mp4"),
                                            ("JPG files", "*.jpg; *.jpeg"),
@@ -56,7 +56,7 @@ class MyFrame(Frame):
                 self.lblTextLoad = Label(self.master,
                                          text=fname,
                                          font=self.normalFont,
-                                         background="SpringGreen3").place(x=200, y=5)
+                                         background=coPrincipal).place(x=200, y=30)
             except:                     # <- naked except is a bad idea
                 showerror("Open Source File", "Failed to read file\n'%s'" % fname)
             return
